47862f53ed5c9ef802f5fa80dcf9f80f8bab7b01
Ryan Heitmann <ryan.heitmann@gmail.com>
2021-03-15 09:28:37 +1300

    Revamping personal page

---
+++ scripts/scripts.js
@@ -0,0 +1,23 @@
+var i = 0;
+var typewriterText = `Hello there, I'm Ryan\nI'm an electrical engineer`;
+var speed = 150;
+
+function typeWriter() {
+    if (i < typewriterText.length) {
+        document.getElementById("journey-text").innerHTML += typewriterText.charAt(i);
+        i++;
+        setTimeout(typeWriter, speed);
+    }
+}
+
+function generateMemoryText() {
+    let memInt = Math.floor(Math.random() * 64) + 32;
+    var memText = `${memInt}K Avail.`
+    document.getElementById("memory").innerHTML ===  memText;
+}
+
+document.addEventListener("DOMContentLoaded", (event) => {
+    console.log('DOM fully loaded.');
+    typeWriter();
+    generateMemoryText();
+});
